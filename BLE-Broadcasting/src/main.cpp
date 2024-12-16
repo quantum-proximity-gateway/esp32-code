6297ec3728fd6f40187824abef2002614f1716ae
Marwan <yassinimarwan6@gmail.com>
2024-12-16 00:22:40 +0000

    Fixed bug of 1 disconnect with BLEServerCallback class

--- BLE-Broadcasting/src/main.cpp
+++ BLE-Broadcasting/src/main.cpp
@@ -2,19 +2,32 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 #define SERVICE_UUID "2246ef74-f912-417f-8530-4a7df291d584"
 #define CHARACTERISTIC_UUID "a3445e11-5bff-4d2a-a3b1-b127f9567bb6"
 
+class MyServer : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        Serial.println("Connected.");
+    }
+
+    void onDisconnect(BLEServer* pServer) {
+        Serial.println("Disconnected.");
+        pServer->getAdvertising()->start();
+    }
+};
+
+
 void setup() {
     Serial.begin(9600);
     Serial.println("Starting BLE Server");
-    
-    BLEDevice::init("MarwanESP32"); // Change this to whatever you need
-    BLEAddress address = BLEDevice::getAddress();
-    Serial.print("Device MAC Address: ");
-    Serial.println(address.toString().c_str());
+    BLEDevice::init("ESP32 Key"); // Change this to whatever you need
+    // BLEAddress address = BLEDevice::getAddress();
+    // Serial.print("Device MAC Address: ");
+    // Serial.println(address.toString().c_str());
     BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServer());
     BLEService *pService = pServer->createService(SERVICE_UUID);
     BLECharacteristic *pCharacteristic = pService->createCharacteristic(
         CHARACTERISTIC_UUID,
@@ -22,11 +35,11 @@ void setup() {
         BLECharacteristic::PROPERTY_WRITE
     );
     
-    pCharacteristic->setValue("HELLO WORLD");
+    pCharacteristic->setValue("{Key: 123554432432, MAC: 2a:23}");
     pService->start();
 
     BLEAdvertisementData oAdvertisementData;
-    oAdvertisementData.setName("Marwan's ESP32 Key");  // Explicitly set the name
+    oAdvertisementData.setName("ESP32 Key");  // Explicitly set the name
     oAdvertisementData.setCompleteServices(BLEUUID(SERVICE_UUID));  // Add service UUID
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->setAdvertisementData(oAdvertisementData);
@@ -36,5 +49,4 @@ void setup() {
 }
 
 void loop() {
-    delay(1000);
 }
