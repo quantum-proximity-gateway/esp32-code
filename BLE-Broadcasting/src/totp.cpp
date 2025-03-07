fd9b3d30a6411d76842aea865d09d818dd223721
Marwan <yassinimarwan6@gmail.com>
2025-03-07 21:16:37 +0000

    removed earlier hashing example

--- BLE-Broadcasting/src/totp.cpp
+++ BLE-Broadcasting/src/totp.cpp
@@ -4,26 +4,6 @@
 #define TIME_STEP 30      // Time step in seconds (commonly 30)
 #define TOTP_DIGITS 6     // Number of digits in OTP
 
-void hash() {
-    const char *message = "Hello, world!";
-    unsigned char output[32];
-    mbedtls_md_context_t ctx;
-    mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
-
-    mbedtls_md_init(&ctx);
-    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 0);
-    mbedtls_md_starts(&ctx);
-    mbedtls_md_update(&ctx, (const unsigned char *)message, strlen(message));
-    mbedtls_md_finish(&ctx, output);
-    mbedtls_md_free(&ctx);
-
-    Serial.print("SHA-256 hash: ");
-    for (int i = 0; i < 32; i++) {
-        Serial.printf("%02x", output[i]);
-    }
-    Serial.println();
-}
-
 unsigned int generate_totp(const unsigned char *key, size_t key_len, uint64_t time_now) {
     uint64_t time_counter = time_now / TIME_STEP;
     unsigned char counter[8];
